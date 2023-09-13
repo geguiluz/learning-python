cddd2c8b5b62b9a833ddb31ddb32210ebc75235c
geguiluz <memo.eguiluz@gmail.com>
2023-09-12 21:05:24 -0600

    Finished esercise 6

--- 6.1.ExerciseLoops.py
+++ 6.1.ExerciseLoops.py
@@ -1,3 +1,4 @@
+x = 0
 numbers = [
     951, 402, 984, 651, 360, 69, 408, 319, 601, 485, 980, 507, 725, 547, 544,
     615, 83, 165, 141, 501, 263, 617, 865, 575, 219, 390, 984, 592, 236, 105, 942, 941,
@@ -10,4 +11,6 @@ numbers = [
 
 # your code goes here
 for number in numbers:
-    print("Do something")
+    x = x+1
+    if number <= 237:
+        print("Iteration: %d, Number: %d" % (x, number))
